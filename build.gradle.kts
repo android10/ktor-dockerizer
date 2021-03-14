b25de590168a7cf8e17fa34f4458414414a2d70d
Fernando Cejas <fcejas@gmail.com>
2021-03-14 11:28:43 +0100

    Change the project structure to adapt to the plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,3 @@
-plugins {
-    java
-    kotlin("jvm") version "1.4.31"
-}
-
-group = "com.fernandocejas"
-version = "0.0.1"
-
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    implementation(kotlin("stdlib"))
-    testImplementation("junit", "junit", "4.12")
 }
---
+++ plugin/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    java
+    kotlin("jvm") version "1.4.31"
+}
+
+group = "com.fernandocejas"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation("junit", "junit", "4.12")
+}
