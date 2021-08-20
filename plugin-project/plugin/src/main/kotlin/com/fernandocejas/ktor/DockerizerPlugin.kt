f4ccfe51bf16bbfc4cd978c65ff7f0a0f2dd1430
Fernando Cejas <fcejas@gmail.com>
2021-08-20 16:15:17 +0200

    Generate fat jar working.

--- plugin-project/plugin/src/main/kotlin/com/fernandocejas/ktor/DockerizerPlugin.kt
+++ plugin-project/plugin/src/main/kotlin/com/fernandocejas/ktor/DockerizerPlugin.kt
@@ -1,25 +1,47 @@
 package com.fernandocejas.ktor
 
+import com.github.jengelman.gradle.plugins.shadow.tasks.*
 import org.gradle.api.*
+
 
 abstract class DockerizerPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         val extension = project.extensions.create(PLUGIN_EXTENSION, DockerizerExtension::class.java)
 
-        setupJar(project, extension)
+        setupShadowJar(project, extension)
         setupDocker(project, extension)
     }
 
     /**
-     * Sets up the generate jar task in order to run Ktor through a fat jar.
+     * Sets up the generate shadow jar task in order to run Ktor through a fat jar.
+     *
+     * It relies on the gradle plugin [EXTERNAL_PLUGIN_SHADOW_JAR].
+     *
+     * @link: https://imperceptiblethoughts.com/shadow/getting-started/
      */
-    private fun setupJar(project: Project, pluginExtension: DockerizerExtension) {
+    private fun setupShadowJar(project: Project, pluginExtension: DockerizerExtension) {
         with(project) {
             plugins.apply(EXTERNAL_PLUGIN_SHADOW_JAR)
-            tasks.register(DockerizerJarTask.TASK_NAME, DockerizerJarTask::class.java) {
-                it.extension.set(pluginExtension)
+
+            project.afterEvaluate {
+                tasks.withType(ShadowJar::class.java).configureEach {
+                    it.archiveBaseName.set("ktor-fernando")
+                    it.archiveClassifier.set("fat")
+                    it.archiveVersion.set("0.0.1")
+                    it.archiveFileName.set("ktor-fernando-fat.jar")
+
+                    it.exclude("META-INF/INDEX.LIST")
+                    it.exclude("META-INF/*.SF")
+                    it.exclude("META-INF/*.DSA")
+                    it.exclude("META-INF/*.RSA")
+                }
+
+                tasks.register(DockerizerJarTask.TASK_NAME, DockerizerJarTask::class.java) {
+                    it.extension.set(pluginExtension)
+                }
             }
+
         }
     }
 
