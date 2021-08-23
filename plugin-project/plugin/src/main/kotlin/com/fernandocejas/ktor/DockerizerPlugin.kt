c61c492342b04ff33284946274977e289e1dcd17
Fernando Cejas <fcejas@gmail.com>
2021-08-23 11:11:59 +0200

    Extract Shadow Plugin logic to a single responsibility class.

--- plugin-project/plugin/src/main/kotlin/com/fernandocejas/ktor/DockerizerPlugin.kt
+++ plugin-project/plugin/src/main/kotlin/com/fernandocejas/ktor/DockerizerPlugin.kt
@@ -1,7 +1,6 @@
 package com.fernandocejas.ktor
 
 import com.fernandocejas.ktor.core.Shadow
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.Exec
@@ -17,37 +16,9 @@ abstract class DockerizerPlugin : Plugin<Project> {
             shadow.applyPlugin()
             docker.setupTasks()
         }
-        setupShadowPlugin(project, extension)
+
+        //TODO: Extract this
         setupDockerTasks(project, extension)
-    }
-
-    /**
-     * Sets up the shadow gradle plugin to be able to generate a fat jar in order to run Ktor
-     * through a fat jar. It relies on the gradle plugin [EXTERNAL_PLUGIN_SHADOW_JAR] and task
-     * [EXTERNAL_TASK_SHADOW_JAR].
-     *
-     * @see: https://imperceptiblethoughts.com/shadow/getting-started/
-     */
-    private fun setupShadowPlugin(project: Project, pluginExtension: DockerizerExtension) {
-        with(project) {
-            afterEvaluate {
-                plugins.apply(EXTERNAL_PLUGIN_SHADOW_JAR)
-
-                tasks.withType(ShadowJar::class.java).configureEach {
-                    it.archiveFileName.set(pluginExtension.jarFilename.get().trim())
-
-                    it.exclude("META-INF/INDEX.LIST")
-                    it.exclude("META-INF/*.SF")
-                    it.exclude("META-INF/*.DSA")
-                    it.exclude("META-INF/*.RSA")
-                }
-
-                tasks.register(DockerizerJarTask.TASK_NAME, DockerizerJarTask::class.java) {
-                    it.extension.set(pluginExtension)
-                    it.dependsOn(EXTERNAL_TASK_SHADOW_JAR)
-                }
-            }
-        }
     }
 
     /**
@@ -85,11 +56,6 @@ abstract class DockerizerPlugin : Plugin<Project> {
     }
 
     companion object {
-        // 3rd Party Gradle Plugin
-        const val EXTERNAL_PLUGIN_SHADOW_JAR = "com.github.johnrengelman.shadow"
-        const val EXTERNAL_TASK_SHADOW_JAR = "shadowJar"
-
-        // Internal Configuration
         const val PLUGIN_EXTENSION = "dockerizer"
         const val TASK_GROUP = "Ktor Dockerizer"
     }
--- plugin-project/plugin/src/main/kotlin/com/fernandocejas/ktor/core/Shadow.kt
+++ plugin-project/plugin/src/main/kotlin/com/fernandocejas/ktor/core/Shadow.kt
@@ -1,11 +1,48 @@
 package com.fernandocejas.ktor.core
 
 import com.fernandocejas.ktor.DockerizerExtension
+import com.fernandocejas.ktor.DockerizerJarTask
+import com.fernandocejas.ktor.DockerizerPlugin
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.api.Project
 
-class Shadow(private val project: Project, extension: DockerizerExtension) {
+/**
+ * Class to handle/manage Ktor Fat Jar creation.
+ * It relies on the 3rd party plugin from John Rengelman
+ * called Shadow.
+ *
+ * @see: https://imperceptiblethoughts.com/shadow/getting-started/
+ */
+class Shadow(private val project: Project, private val extension: DockerizerExtension) {
 
+    val pluginExcludes = setOf("")
+
+    /**
+     * Sets up the shadow gradle plugin to be able to generate a fat jar in
+     * order to run Ktor through a jar with all the runtime dependencies.
+     */
     fun applyPlugin() {
+        with(project) {
+            plugins.apply(PLUGIN_SHADOW_JAR)
 
+            tasks.withType(ShadowJar::class.java).configureEach {
+                it.archiveFileName.set(extension.jarFilename.get().trim())
+
+                it.exclude("META-INF/INDEX.LIST")
+                it.exclude("META-INF/*.SF")
+                it.exclude("META-INF/*.DSA")
+                it.exclude("META-INF/*.RSA")
+            }
+
+            tasks.register(DockerizerJarTask.TASK_NAME, DockerizerJarTask::class.java) {
+                it.extension.set(extension)
+                it.dependsOn(TASK_SHADOW_JAR)
+            }
+        }
+    }
+
+    companion object {
+        const val PLUGIN_SHADOW_JAR = "com.github.johnrengelman.shadow"
+        const val TASK_SHADOW_JAR = "shadowJar"
     }
 }
