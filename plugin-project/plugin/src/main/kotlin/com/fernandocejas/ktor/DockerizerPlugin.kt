4f5edad9800b07e18b9ae5814c0e427418515707
Fernando Cejas <fcejas@gmail.com>
2021-09-03 17:31:31 +0200

    ref: move docker tasks to proper Docker class.

--- plugin-project/plugin/src/main/kotlin/com/fernandocejas/ktor/DockerizerPlugin.kt
+++ plugin-project/plugin/src/main/kotlin/com/fernandocejas/ktor/DockerizerPlugin.kt
@@ -1,57 +1,20 @@
 package com.fernandocejas.ktor
 
+import com.fernandocejas.ktor.core.Docker
 import com.fernandocejas.ktor.core.Shadow
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.Exec
 
 abstract class DockerizerPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         val extension = project.extensions.create(PLUGIN_EXTENSION, DockerizerExtension::class.java)
         val shadow = Shadow(project, extension)
-        val docker = com.fernandocejas.ktor.core.Docker(project, extension)
+        val docker = Docker(project, extension)
 
         project.afterEvaluate {
             shadow.applyPlugin()
             docker.setupTasks()
-        }
-
-        //TODO: Extract this
-        setupDockerTasks(project, extension)
-    }
-
-    /**
-     * Sets up the docker task in order to run Ktor inside a docker container.
-     */
-    private fun setupDockerTasks(project: Project, pluginExtension: DockerizerExtension) {
-        with(project) {
-            afterEvaluate {
-                tasks.register(DockerizerDockerTask.TASK_NAME, DockerizerDockerTask::class.java) {
-                    it.extension.set(pluginExtension)
-                    it.dependsOn(Shadow.GENERATE_JAR_TASK_NAME)
-                }
-
-                tasks.register("dockerBuildImage", Exec::class.java) {
-//                    group = Docker.GROUP
-                    description = "Builds a docker image containing the Ktor Application."
-                    it.commandLine(Docker.Commands.buildExec(Docker.Commands.BUILD))
-                    it.dependsOn(Shadow.GENERATE_JAR_TASK_NAME)
-                }
-
-//                tasks.register("dockerRun", Exec::class.java) { execTask ->
-                tasks.register("dockerRun") { execTask ->
-//                    group = "Docker.GROUP"
-                    description = "Runs App inside a Docker Container."
-                    execTask.doLast {
-                        exec {
-                            it.commandLine(Docker.Commands.buildExec(Docker.Commands.RUN_ATTACHED))
-                        }
-                    }
-//                    it.commandLine = listOf()
-//                    commandLine(Docker.Commands.buildExec(Docker.Commands.RUN_ATTACHED))
-                }
-            }
         }
     }
 
--- plugin-project/plugin/src/main/kotlin/com/fernandocejas/ktor/core/Docker.kt
+++ plugin-project/plugin/src/main/kotlin/com/fernandocejas/ktor/core/Docker.kt
@@ -1,11 +1,78 @@
 package com.fernandocejas.ktor.core
 
+import com.fernandocejas.ktor.DockerizerDockerTask
 import com.fernandocejas.ktor.DockerizerExtension
 import org.gradle.api.Project
+import org.gradle.api.tasks.Exec
 
-class Docker(private val project: Project, extension: DockerizerExtension) {
+class Docker(private val project: Project, private val extension: DockerizerExtension) {
 
+    object Config {
+        const val MEMORY = "512M"
+        const val CPUS = "1"
+        const val HOST_PORT = 80
+        const val HOST_SSL_PORT = 443
+        const val CONTAINER_PORT = 5000
+        const val CONTAINER_SSL_PORT = 8443
+        const val ARTIFACT = "ktor-fat-jar.jar"
+    }
+
+    object Commands {
+        private const val DELIMITER = " "
+        private const val RUN_PARAMS =
+            "-m${Config.MEMORY} " +
+                    "--cpus ${Config.CPUS} -t " +
+                    "-p ${Config.HOST_PORT}:${Config.CONTAINER_PORT} " +
+                    "-p ${Config.HOST_SSL_PORT}:${Config.CONTAINER_SSL_PORT} " +
+                    "-p ${Config.CONTAINER_SSL_PORT}:${Config.CONTAINER_SSL_PORT} " +
+                    "--rm ${Config.ARTIFACT}"
+
+        private val RUN = "docker run --name=${Config.ARTIFACT}"
+
+        const val BUILD = "docker build -t ${Config.ARTIFACT} ."
+        val RUN_ATTACHED = RUN.plus(DELIMITER).plus(RUN_PARAMS)
+        val RUN_DETACHED = RUN.plus(DELIMITER).plus("-d").plus(DELIMITER).plus(RUN_PARAMS)
+        const val STOP_CONTAINER = "docker stop ${Config.ARTIFACT}"
+        const val LIST_IMAGES = "docker image ls"
+        const val LIST_CONTAINERS = "docker ps -a"
+        const val REMOVE_IMAGE = "docker image rm -f ${Config.ARTIFACT}"
+        const val REMOVE_DANGLING_IMAGES = "docker image prune --filter=dangling=true -f"
+
+        fun buildExec(command: String) = command.split(DELIMITER)
+//        fun log(command: String) = ""
+    }
+
+    /**
+     * Sets up the docker task in order to run Ktor inside a docker container.
+     */
     fun setupTasks() {
+        with(project) {
+            afterEvaluate {
+                tasks.register(DockerizerDockerTask.TASK_NAME, DockerizerDockerTask::class.java) {
+                    it.extension.set(this@Docker.extension)
+                    it.dependsOn(Shadow.GENERATE_JAR_TASK_NAME)
+                }
 
+                tasks.register("dockerBuildImage", Exec::class.java) {
+//                    group = Docker.GROUP
+                    description = "Builds a docker image containing the Ktor Application."
+                    it.commandLine(Commands.buildExec(Commands.BUILD))
+                    it.dependsOn(Shadow.GENERATE_JAR_TASK_NAME)
+                }
+
+//                tasks.register("dockerRun", Exec::class.java) { execTask ->
+                tasks.register("dockerRun") { execTask ->
+//                    group = "Docker.GROUP"
+                    description = "Runs App inside a Docker Container."
+                    execTask.doLast {
+                        exec {
+                            it.commandLine(Commands.buildExec(Commands.RUN_ATTACHED))
+                        }
+                    }
+//                    it.commandLine = listOf()
+//                    commandLine(Docker.Commands.buildExec(Docker.Commands.RUN_ATTACHED))
+                }
+            }
+        }
     }
 }
