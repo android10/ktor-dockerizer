a968a0cb80d9f818e7d93cf3e90400cec51ebf25
Fernando Cejas <fcejas@gmail.com>
2021-09-13 13:02:16 +0200

    ref: organize docker tasks.

--- plugin-project/plugin/src/main/kotlin/com/fernandocejas/ktor/core/Docker.kt
+++ plugin-project/plugin/src/main/kotlin/com/fernandocejas/ktor/core/Docker.kt
@@ -8,16 +8,52 @@ import org.gradle.api.tasks.Exec
 
 class Docker(private val project: Project, private val extension: DockerizerExtension) {
 
-    private val config = Config(extension)
+    private fun createTaskList(): List<Task> {
+        val dockerConfig = Config(extension)
+        val dockerCommands = Command(dockerConfig)
+
+        return listOf(
+            Task(project,
+                name = "dockerBuildImage",
+                description = "Builds a docker image containing the Ktor Application.",
+                command = dockerCommands.build,
+                dependsOn = Shadow.GENERATE_JAR_TASK_NAME),
+
+            Task(project,
+                name = "dockerRun",
+                description = "Runs App inside a Docker Container.",
+                command = dockerCommands.runAttached),
+
+            Task(project,
+                name = "dockerRunDetached",
+                description = "Runs App inside a Docker Container Detached.",
+                command = dockerCommands.runDetached),
+        )
+    }
+
+    /**
+     * Sets up the docker task in order to run Ktor inside a docker container.
+     */
+    fun setupTasks() = createTaskList().map { it.register() }
+
+    private class Config(private val extension: DockerizerExtension) {
+        val dockerImageName: String = extension.dockerImageName.get()
+        val dockerMemory: String = extension.dockerMemory.get()
+        val dockerCpus: String = extension.dockerCpus.get()
+        val dockerHostPort: Int = extension.dockerHostPort.get()
+        val dockerHostSslPort: Int = extension.dockerHostSslPort.get()
+        val dockerContainerPort: Int = extension.dockerContainerPort.get()
+        val dockerContainerSslPort: Int = extension.dockerContainerSslPort.get()
+    }
 
     private class Command(private val config: Config) {
         private val runParams =
             "-m${config.dockerMemory} " +
-            "--cpus ${config.dockerCpus} -t " +
-            "-p ${config.dockerHostPort}:${config.dockerContainerPort} " +
-            "-p ${config.dockerHostSslPort}:${config.dockerContainerSslPort} " +
-            "-p ${config.dockerContainerSslPort}:${config.dockerContainerSslPort} " +
-            "--rm ${config.dockerImageName}"
+                    "--cpus ${config.dockerCpus} -t " +
+                    "-p ${config.dockerHostPort}:${config.dockerContainerPort} " +
+                    "-p ${config.dockerHostSslPort}:${config.dockerContainerSslPort} " +
+                    "-p ${config.dockerContainerSslPort}:${config.dockerContainerSslPort} " +
+                    "--rm ${config.dockerImageName}"
 
         private val run = "docker run --name=${config.dockerImageName}"
 
@@ -33,57 +69,17 @@ class Docker(private val project: Project, private val extension: DockerizerExte
         private fun buildExec(command: String) = command.split(String.DELIMITER)
     }
 
-    /**
-     * Sets up the docker task in order to run Ktor inside a docker container.
-     */
-    fun setupTasks() {
-        with(project) {
-            tasks.register("dockerBuildImage", Exec::class.java) {
-                group = DockerizerPlugin.TASK_GROUP
-                description = "Builds a docker image containing the Ktor Application."
-                it.commandLine(Commands.buildExec(Commands.BUILD))
-                it.dependsOn(Shadow.GENERATE_JAR_TASK_NAME)
-            }
-
-            tasks.register("dockerRun", Exec::class.java) {
-                group = DockerizerPlugin.TASK_GROUP
-                description = "Runs App inside a Docker Container."
-                it.commandLine(Commands.buildExec(Commands.RUN_ATTACHED))
-            }
-
-            tasks.register("dockerRunDetached", Exec::class.java) {
-                group = DockerizerPlugin.TASK_GROUP
-                description = "Runs App inside a Docker Container Detached."
-                it.commandLine(Commands.buildExec(Commands.RUN_DETACHED))
-            }
-        }
-    }
-
-    private class Config(private val extension: DockerizerExtension) {
-        val dockerImageName: String = extension.dockerImageName.get()
-        val dockerMemory: String = extension.dockerMemory.get()
-        val dockerCpus: String = extension.dockerCpus.get()
-        val dockerHostPort: Int = extension.dockerHostPort.get()
-        val dockerHostSslPort: Int = extension.dockerHostSslPort.get()
-        val dockerContainerPort: Int = extension.dockerContainerPort.get()
-        val dockerContainerSslPort: Int = extension.dockerContainerSslPort.get()
-    }
-
     private class Task(private val project: Project, private val name: String,
-               private val description: String, private val command: String,
+               private val description: String, private val command: List<String>,
                private val dependsOn: String = String.EMPTY) {
 
         fun register() {
             project.tasks.register(name, Exec::class.java) {
-                it.group = GROUP
+                it.group = DockerizerPlugin.TASK_GROUP
                 it.description = description
                 it.commandLine(command)
-                it.dependsOn(dependsOn)
+                if (dependsOn != String.EMPTY) it.dependsOn(dependsOn)
             }
-        }
-
-        companion object {
-            const val GROUP = DockerizerPlugin.TASK_GROUP
         }
     }
 }
