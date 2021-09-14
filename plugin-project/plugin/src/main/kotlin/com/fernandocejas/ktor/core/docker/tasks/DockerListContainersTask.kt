daed4b30732e3d5fe00851f20b84c485139100cd
Fernando Cejas <fcejas@gmail.com>
2021-09-14 20:15:42 +0200

    feat: add docker list running containers gradle task utility.

---
+++ plugin-project/plugin/src/main/kotlin/com/fernandocejas/ktor/core/docker/tasks/DockerListContainersTask.kt
@@ -0,0 +1,13 @@
+package com.fernandocejas.ktor.core.docker.tasks
+
+import com.fernandocejas.ktor.DockerizerExtension
+import com.fernandocejas.ktor.core.docker.tasks.Task.Command.Companion.fromString
+import org.gradle.api.Project
+
+class DockerListContainersTask(project: Project, extension: DockerizerExtension) : Task(project, extension) {
+
+    // Task overridden fields
+    override val name = "dockerListContainers"
+    override val description = "List all the running Docker Containers."
+    override val command = fromString("docker ps -a")
+}
